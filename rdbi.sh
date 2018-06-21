8a359c86a9dd958a5c9aafb20cd225e7f2e00e3e
Utsuho <17104706+prixyn@users.noreply.github.com>
2018-06-21 00:24:06 -0700

    Support Ubuntu & Debian

---
+++ rdbi.sh
@@ -0,0 +1,22 @@
+# RethinkDB Linux Installation Script
+#   by RethinkDB
+#   Globalized by TRPGDA (Prixyn)
+
+
+if [ -f /etc/lsb-release ]; then
+  # Debian/Ubuntu
+  if [[ $(cat /etc/issue) = *Ubuntu* ]]; then
+    source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+    wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install rethinkdb
+  else
+    echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+    wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install rethinkdb
+  fi
+else
+  echo "Linux Distribution not Supported!"
+  exit
+fi
