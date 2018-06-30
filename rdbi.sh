845d85c074d3bf0bfca21a91d123a110f9102a8f
Utsuho <17104706+prixyn@users.noreply.github.com>
2018-06-30 13:02:29 -0700

    Update rdbi.sh

--- rdbi.sh
+++ rdbi.sh
@@ -6,11 +6,13 @@
 if [ -f /etc/lsb-release ]; then
   # Debian/Ubuntu
   if [[ $(cat /etc/issue) = *Ubuntu* ]]; then
+    echo "⯈ Installing Ubuntu"
     source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
     wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
     sudo apt-get update
     sudo apt-get install rethinkdb
   else
+    echo "⯈ Installing Debian"
     echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
     wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
     sudo apt-get update
