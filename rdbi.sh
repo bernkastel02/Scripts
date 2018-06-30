29a33e795b481da4ef97bf1d33036ba4d02e5bbb
Utsuho <17104706+prixyn@users.noreply.github.com>
2018-06-30 13:06:18 -0700

    add -y

--- rdbi.sh
+++ rdbi.sh
@@ -10,13 +10,13 @@ if [ -f /etc/lsb-release ]; then
     source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
     wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
     sudo apt-get update
-    sudo apt-get install rethinkdb
+    sudo apt-get install -y rethinkdb
   else
     echo "⯈ Installing RethinkDB for Debian"
     echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
     wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
     sudo apt-get update
-    sudo apt-get install rethinkdb
+    sudo apt-get install -y rethinkdb
   fi
 else
   echo "Linux Distribution not Supported!"
