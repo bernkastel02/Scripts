192361c137776faaba8b97aeab4838bf17eb7cc0
Wakasagihime <17104706+dekasugihime@users.noreply.github.com>
2018-12-12 21:42:40 -0800

    Fedora & Raspbian

--- rdbi.sh
+++ rdbi.sh
@@ -6,13 +6,24 @@
 
 
 if [ -f /etc/lsb-release ]; then
-  # Debian/Ubuntu
+  # Debian, Ubuntu & Raspbian
   if [[ $(cat /etc/issue) = *Ubuntu* ]]; then
     echo "⯈ Installing RethinkDB for Ubuntu"
     source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
     wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
     sudo apt-get update
     sudo apt-get install -y rethinkdb
+  elif [[ $(cat /etc/os-release) = *Raspbian* ]]; then
+		# Gotta build from source.
+  	echo "⯈ Installing RethinkDB for Raspbian"
+		sudo apt-get install g++ protobuf-compiler libprotobuf-dev \
+                     libboost-dev curl m4 wget libssl-dev
+		wget https://download.rethinkdb.com/dist/rethinkdb-2.3.6.tgz
+		tar xf rethinkdb-2.3.6.tgz
+		cd rethinkdb-2.3.6
+		./configure --with-system-malloc --allow-fetch
+		make ALLOW_WARNINGS=1
+		sudo make install ALLOW_WARNINGS=1
   else
     echo "⯈ Installing RethinkDB for Debian"
     echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
@@ -36,6 +47,11 @@ elif [ -f /etc/redhat-release ]; then
     sudo yum -y update
     sudo yum -y install rethinkdb
 			;;
+		"Fedora release "*)
+			sudo wget https://download.rethinkdb.com/centos/6/`uname -m`/rethinkdb.repo -O /etc/yum.repos.d/rethinkdb.repo
+			sudo yum -y update
+			sudo yum -y install rethinkdb
+		;;
 		*)
 			# Unsupported RHEL Version
 			echo "RHEL Version not supported!"
